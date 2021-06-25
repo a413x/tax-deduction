62b1dd4fafc52ffa5c3eb7cf54c6c46b968b55c2
a413x <variuhin@yandex.ru>
2021-06-25 16:36:22 +0300

    Add app styles

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  #app{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Lab Grotesque;
+    font-style: normal;
+    background: $gradient-red;
+    box-shadow: 0px -0.11px 16.9495px rgba(183, 187, 225, 0.33);
+    border-radius: 0px;
+  }
 </style>
