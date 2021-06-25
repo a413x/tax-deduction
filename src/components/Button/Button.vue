2a2b55431e06ac566be56475e7a046a97aa61db7
a413x <variuhin@yandex.ru>
2021-06-25 16:28:07 +0300

    Add button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="button" :disabled="disabled">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: { disabled: Boolean }
+  }
+</script>
+
+<style lang="scss" scoped>
+.button{
+  font-weight: 500;
+  cursor: pointer;
+  border-radius: 6px;
+  color: white;
+}
+.button:disabled, .button:disabled:hover{
+  background: $gray;
+  color: white;
+  border: none;
+  cursor: auto;
+}
+</style>
