f34a947e54bdfb97f99501f367be4be7a553b725
Alex <variuhin@yandex.ru>
2021-07-05 11:52:05 +0300

    Add close button component

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="close-button" @click="$emit('close')">
+    <img src="@/assets/close.svg"/>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.close-button{
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
