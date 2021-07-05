b8161cae87255ed8ed13b045f10708a9e27608f4
Alex <variuhin@yandex.ru>
2021-07-05 11:56:49 +0300

    Add popup component

---
+++ src/components/Popup/Popup.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="popup" v-if="show">
+    <div class="popup__container">
+      <div class="popup__inner">
+        <CloseButton class="popup__close-button" @close="$emit('close')"/>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CloseButton from '../CloseButton.vue'
+export default {
+  props: { show: Boolean },
+  components: { CloseButton }
+}
+</script>
+
+<style lang="scss" scoped>
+  .popup{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: white;
+  }
+  .popup__container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, .3)
+  }
+  .popup__inner{
+    position: relative;
+    padding: 32px;
+    background: white;
+    border-radius: 30px;
+  }
+  .popup__close-button{
+    position: absolute;
+    top: 16px;
+    right: 16px;
+  }
+</style>
