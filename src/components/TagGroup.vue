40e177e2c392394e26702f81aae5e92214eed065
Alex <variuhin@yandex.ru>
2021-07-07 15:13:48 +0300

    Create a tag group with the ability to toggle the active state

---
+++ src/components/TagGroup.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tag-group">
+    <Tag
+      v-for="(tag, i) in tags"
+      :key="i"
+      :active="activeIndex===i"
+      class="tag-group__tag"
+      @click="()=>onChange(i)"
+    >
+      {{tag.title}}
+    </Tag>
+  </div>
+</template>
+
+<script>
+import Tag from './Tag.vue'
+export default {
+  props: { tags: Array },
+  data(){
+    return { activeIndex: 0 }
+  },
+  components: { Tag },
+  methods: {
+    onChange(i){
+      this.activeIndex = i
+      this.$emit('change', i)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tag-group__tag{
+  margin-left: 16px;
+}
+</style>
