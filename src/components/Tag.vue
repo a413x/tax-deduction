8de4b0201d1a73523d8cdbabf1d444175cc9c3ed
Alex <variuhin@yandex.ru>
2021-07-07 15:03:09 +0300

    Add tag component

---
+++ src/components/Tag.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    class="tag"
+    :class="{'tag_not-active': !active}"
+    @click="$emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: { active: Boolean }
+}
+</script>
+
+<style lang="scss" scoped>
+.tag{
+  @include flex-center;
+  display: inline-flex;
+  font-weight: normal;
+  padding: 6px 12px;
+  background: $gradient-red;
+  border-radius: 50px;
+  border: none;
+  color: white;
+  cursor: pointer;
+}
+.tag:hover{
+  background: #DFE3E6;
+  color: black;
+}
+.tag_not-active{
+  background: #EEF0F2;
+  color: black;
+}
+</style>
