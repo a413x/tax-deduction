db463c0dc482184aba17f9e389108cc9958f9dee
Alex <variuhin@yandex.ru>
2021-07-07 15:14:50 +0300

    Add text blocks for tax deduction component

---
+++ src/components/TaxDeduction/TaxDeduction__info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tax-deduction__info">
+    Используйте налоговый вычет чтобы погасить ипотеку досрочно.<br>
+    Размер налогового вычета составляет не более 13% от своего<br>
+    официального годового дохода.
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tax-deduction__info{
+  margin-bottom: 24px;
+  font-weight: normal;
+  color: #808080;
+}
+</style>
---
+++ src/components/TaxDeduction/TaxDeduction__title.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="tax-deduction__title">Налоговый вычет</div>
+</template>
+
+<style lang="scss" scoped>
+.tax-deduction__title{
+  margin-bottom: 16px;
+  font-weight: 500;
+  font-size: 28px;
+  line-height: 40px;
+}
+</style>
