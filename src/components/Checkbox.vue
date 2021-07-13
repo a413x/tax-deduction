4798f3f74a4cb4041f6b8865d502e33a33f6364f
Alex <variuhin@yandex.ru>
2021-07-13 12:52:16 +0300

    Add checkbox

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,52 @@
+<template>
+  <label class="checkbox">
+    <input
+      type="checkbox"
+      @change="$emit('input', $event.target.checked)"
+      :disabled="disabled"
+    >
+    <span class="checkbox__input">
+      <img src="@/assets/checkmark.svg" class="checkbox__checkmark"/>
+    </span>
+  </label>
+</template>
+
+<script>
+export default{
+  props: { disabled: Boolean }
+}
+</script>
+
+<style lang="scss" scoped>
+.checkbox{
+  display:inline-block;
+  width: 20px;
+}
+.checkbox input{
+  display: none;
+  width: 20px;
+  margin: 0;
+  &:checked ~ .checkbox__input{
+    background: $gradient-red;
+    border: none;
+  }
+  &:disabled ~ .checkbox__input{
+    background: $gray;
+    border: none;
+    cursor: auto;
+  }
+}
+.checkbox__input{
+  @include flex-center;
+  width: 20px;
+  height: 20px;
+  border: 1px solid #DFE3E6;
+  box-sizing: border-box;
+  border-radius: 6px;
+  cursor: pointer;
+  user-select: none;
+  &:hover{
+    border-color: black;
+  }
+}
+</style>
