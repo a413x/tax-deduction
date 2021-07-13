6a4d28c6ef1c2a3bb49d1341fc1cf7cc3aee4cad
Alex <variuhin@yandex.ru>
2021-07-13 16:17:38 +0300

    Input component refactoring and styles adjustment

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -6,12 +6,12 @@
       class="input__input"
       :placeholder="placeholder"
       :disabled="disabled"
-      :class="{'input_error': !valid}"
+      :class="{'input__input_error': !valid}"
       @input="$emit('input', $event)"
       @focus="$emit('focus', $event)"
       @blur="$emit('blur', $event)"
     />
-    <div class="input__error" v-if="!valid">
+    <div class="input__error-msg" v-if="!valid">
       Поле обязательно для заполнения
     </div>
   </div>
@@ -31,33 +31,35 @@ export default {
 <style lang="scss" scoped>
 .input__label{
   font-weight: 500;
-  font-size: 14px;
-  line-height: 24px;
 }
 .input__input{
   width: 100%;
   height: 40px;
   display: block;
-  margin: 8px 0px 4px 0px;
+  margin-top: 8px;
   padding: 8px 10px;
-  border: 1px solid #DFE3E6;
+  border: 1px solid $gray800;
   box-sizing: border-box;
   border-radius: 3px;
+  &::placeholder{
+    color: $gray900;
+  }
+  &:hover{
+    border-color: black;
+  }
+  &:focus{
+    border-color: $gray800;
+    outline: none;
+  }
+  &:disabled{
+    color: #808080;
+    border-color: #808080;
+  }
 }
-.input__input::placeholder{
-  color:#BEC5CC;
-}
-.input__input:hover{
-  border-color: black;
-}
-.input__input:focus{
-  border-color: #DFE3E6;
-  outline: none;
-}
-.input_error{
+.input__input_error{
   border-color: $red;
 }
-.input__error{
+.input__error-msg{
   font-size: 10px;
   line-height: 12px;
   color: $red;
